e829956f4ea32d3e943afc5579262a272b2b6bad
Meenakshi <mogilimeenakshi116@gmail.com>
2026-08-16 00:40:19 +0530

    :wqAllow deployed frontend in CORS
    :wq
    :wq#

--- backend/main.py
+++ backend/main.py
@@ -6,11 +6,14 @@ app = FastAPI(title="SkillGraph API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://skillgraph-frontend-u32z.onrender.com"
-    ],
+       allow_origins=[
+
+    "http://localhost:5173",
+"http://127.0.0.1:5173",
+"https://skillgraph-frontend-u32z.onrender.com"
+
+],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
